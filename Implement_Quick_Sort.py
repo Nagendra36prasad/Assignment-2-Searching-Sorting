83469cf0f52d028b6810f0a0734ef80a5dc97b07
Nagendra36prasad <122007988+Nagendra36prasad@users.noreply.github.com>
2023-03-16 23:18:53 +0530

    Implement Quick Sort

---
+++ Implement_Quick_Sort.py
@@ -0,0 +1,15 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+
+    for num in arr[1:]:
+        if num < pivot:
+            left.append(num)
+        else:
+            right.append(num)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
